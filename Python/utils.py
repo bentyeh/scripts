2ba272fe6e7860e9628b33e0a25840faf31bd60d
Benjamin Yeh <bentyeh@users.noreply.github.com>
2019-04-23 23:31:07 -0700

    Python/utils.py: new function findOverlapIntervals()

--- Python/utils.py
+++ Python/utils.py
@@ -1,3 +1,6 @@
+import itertools
+import numpy as np
+
 def isint(s):
     '''
     Check if a string represents an int.
@@ -16,7 +19,7 @@ def isfloat(s):
     - To determine whether a string `s` represents an int or a float, consider the following options:
       1. Use `isint(s)` first, then `isfloat(s)` if the former returns False.
       2. Use `isfloat()` first, then `s.is_integer()` if the former returns True.
-    
+
     Source: https://stackoverflow.com/a/15357477
     '''
     try:
@@ -24,3 +27,37 @@ def isfloat(s):
     except ValueError:
         return False
     return True
+
+def findOverlapIntervals(intervals, n):
+    '''
+    Find regions overlapped by n or more intervals.
+
+    Args
+    - intervals: list of 2-tuples of real numbers (float or int)
+        Pythonic intervals [start, end), where start < end.
+    - n: int
+        The output regions will be overlapped by at least `n` of the intervals given in `Intervals`.
+
+    Returns: list of 2-tuples of real numbers (float or int)
+
+    Algorithm based on checking for balanced parentheses. Credits to Zachary Taylor for the idea.
+    '''
+
+    values = np.array(list(itertools.chain.from_iterable(intervals)))
+    starts = np.array([1, -1]*len(intervals)) # 1: start value; -1: end value
+    idx = np.argsort(values)
+    values = values[idx]
+    starts = starts[idx]
+    counts = np.cumsum(starts)
+    highCountsIdx = np.flatnonzero(counts >= n)
+
+    regions = []
+    start = None
+    for i in range(len(highCountsIdx)):
+        if start is None:
+            start = highCountsIdx[i]
+
+        if (i == len(highCountsIdx) - 1) or (highCountsIdx[i+1] - highCountsIdx[i] > 1):
+            regions.append((values[start], values[highCountsIdx[i]+1]))
+            start = None
+    return regions
