7a28720f970ad6b6da99ab1f29e1c80d1eb10e51
Benjamin Yeh <bentyeh@users.noreply.github.com>
2019-04-27 01:04:56 -0700

    Python/utils.py: new function weightedOverlapIntervals()
    weightedOverlapIntervals() generalizes findOverlapIntervals() (now deprecated) to (1) account for weights associated with input intervals and (2)also return indices of the original intervals contributing to the new intervals

--- Python/utils.py
+++ Python/utils.py
@@ -28,36 +28,67 @@ def isfloat(s):
         return False
     return True
 
-def findOverlapIntervals(intervals, n):
+def weightedOverlapIntervals(intervals):
     '''
-    Find regions overlapped by n or more intervals.
+    Return non-overlapping set of regions whose weights are the sum of the weights of
+    overlapping input intervals spanning that region
 
     Args
-    - intervals: list of 2-tuples of real numbers (float or int)
-        Pythonic intervals [start, end), where start < end.
-    - n: int
-        The output regions will be overlapped by at least `n` of the intervals given in `Intervals`.
+    - intervals: list of 2- or 3-tuples of real numbers
+        (start, end[, weight]) where start < end
+        weight assumed to be 1 if not provided
 
-    Returns: list of 2-tuples of real numbers (float or int)
-
-    Algorithm based on checking for balanced parentheses. Credits to Zachary Taylor for the idea.
+    Returns: 2-tuple of (list of 3-tuples of real numbers, list of sets of integers)
+    - Element 0: list of non-overlapping intervals (start, end, weight) where start < end
+      - These non-overlapping intervals span the entire range of the input intervals.
+        Regions in this range not overlapped by at least 1 input interval are included
+        with weight 0.
+    - Element 1: list of sets of indices of original intervals contributing to new intervals
     '''
+    n = len(intervals)
 
-    values = np.array(list(itertools.chain.from_iterable(intervals)))
-    starts = np.array([1, -1]*len(intervals)) # 1: start value; -1: end value
-    idx = np.argsort(values)
-    values = values[idx]
-    starts = starts[idx]
-    counts = np.cumsum(starts)
-    highCountsIdx = np.flatnonzero(counts >= n)
+    values = np.array(intervals)
+    s = values[:,0]
+    e = values[:,1]
+    if values.shape[1] != 3:
+        w = np.ones(s.shape)
+    else:
+        w = values[:,2]
 
-    regions = []
-    start = None
-    for i in range(len(highCountsIdx)):
-        if start is None:
-            start = highCountsIdx[i]
+    s_argsort = np.argsort(s)
+    s_sorted = s[s_argsort]
+    e_argsort = np.argsort(e)
+    e_sorted = e[e_argsort]
 
-        if (i == len(highCountsIdx) - 1) or (highCountsIdx[i+1] - highCountsIdx[i] > 1):
-            regions.append((values[start], values[highCountsIdx[i]+1]))
-            start = None
-    return regions
+    start = s_sorted[0]      # current start
+    weight = w[s_argsort[0]] # current weight
+    s_idx = 1
+    e_idx = 0
+
+    newIntervals = []     # list of non-overlapping intervals
+    overlapIntervals = [] # list of sets of indices of original intervals contributing to new intervals
+    curOverlap = set([s_argsort[0]])
+
+    while e_idx < n:
+        if (s_idx < n) and (s_sorted[s_idx] < e_sorted[e_idx]):
+            end = s_sorted[s_idx]
+            newIntervals.append((start, end, weight))
+            overlapIntervals.append(set(curOverlap))
+            weight += w[s_argsort[s_idx]]
+            curOverlap.add(s_argsort[s_idx])
+            s_idx += 1
+        else:
+            end = e_sorted[e_idx]
+            overlapIntervals.append(set(curOverlap))
+            newIntervals.append((start, end, weight))
+            weight -= w[e_argsort[e_idx]]
+            curOverlap.remove(e_argsort[e_idx])
+            e_idx += 1
+        start = end
+    
+    # post-processing: remove length-0 intervals where start == end
+    validIntervals = [i for i in range(len(newIntervals)) if newIntervals[i][0] < newIntervals[i][1]]
+    newIntervals = [newIntervals[i] for i in validIntervals]
+    overlapIntervals = [overlapIntervals[i] for i in validIntervals]
+    
+    return (newIntervals, overlapIntervals)
