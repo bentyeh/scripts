a51975ee71a5312be24843171ed8d693c3c80b2f
Benjamin Yeh <bentyeh@users.noreply.github.com>
2019-05-16 03:45:46 -0700

    Python/utils_files.py: new createValidPath() function

--- Python/utils_files.py
+++ Python/utils_files.py
@@ -1,18 +1,41 @@
-import gzip
+import gzip, os, re
+
+def createValidPath(s, sub_spaces=None):
+    '''
+    Create valid path name.
+    - Remove characters that are not alphanumeric, space, underscore, dash, or period
+    - [Optional] Remove whitespace
+    - Normalize pathname
+
+    Args
+    - s: str
+        String to convert to valid path
+    - sub_spaces: str. default=None
+        String with which to replace whitespace, e.g., '_'
+
+    Return: str
+
+    Note: Inspired by https://stackoverflow.com/a/295466 and django's get_valid_filename()
+      https://github.com/django/django/blob/97d3321e89c8d4434927bdbc308db1ccffa99d3b/django/utils/text.py#L221
+    '''
+    s = re.sub(r'(?u)[^-\w. ]', '', s.strip()).strip()
+    if sub_spaces is not None:
+        s = re.sub(r' ', sub_spaces, s).strip()
+    return os.path.normpath(s)
 
 def createFileObject(file, mode=None):
     '''
     Create file object.
-    
+
     Args
     - file: str
         Path to file. If extension ends with '.gz', gzip compression is assumed.
     - mode: str. default=None
         Mode to open file with.
-    
+
     Returns: file object (io.TextIOBase)
     '''
-    
+
     assert(type(file) is str)
     if file.endswith('.gz'):
         mode = mode if mode is not None else 'rt'
@@ -33,11 +56,11 @@ def readFile(file, strip=None):
         Parameter to str.strip() applied to each read line.
         - None: strip whitespace
         - '': do not strip
-    
+
     Returns: list of str
       Each element in the list is a line from the file.
     '''
-    
+
     with createFileObject(file) as f:
         contents = f.read().strip(strip).splitlines()
     return contents
